8b4dcfe6404d9d0d89974f9dd2d0679e4f5dfb74
Citybilly <zeor7002@gmail.com>
2021-10-17 21:57:02 +0900

    211017

---
+++ 000_main/Cpp_Basic/Chapter03/RacingMain.cpp
@@ -0,0 +1,16 @@
+//#include "/Users/woohyun/000_Github/001_CoTeStudy-C/000_main/Cpp_Basic/Chapter03/Car.h"
+#include <iostream>
+#include "Car.h"
+
+int main(void)
+{
+    Car run99;
+    run99.InitMembers("run99", 100);
+//    run99.Accel();
+//    run99.Accel();
+//    run99.Accel();
+//    run99.ShowCarState();
+//    run99.Break();
+//    run99.ShowCarState();
+    return 0;
+}
